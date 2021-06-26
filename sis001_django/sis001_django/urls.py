56713c4f8b6f4c70fa63f418af760669b98fa18d
498330580 <498330580@qq.com>
2021-06-26 10:19:53 +0800

    放弃flask后端，改用drf作为后端

--- sis001_django/sis001_django/urls.py
+++ sis001_django/sis001_django/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+# router.register(r'users', UsersViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path(r'api/doc/', include_docs_urls(title='人员资产管理系统PAMS')),     # api测试接口
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
